2f992cd4c2a41be9d8056a9afceed6a702379153
sergey <sergey@comoyo.com>
2013-12-20 11:20:53 +0100

    js to push widgets to conversation

---
+++ js/insert.js
@@ -0,0 +1,3 @@
+function ins_wow(dataRef, localMsisdn, partnerMsisdn) {
+     conversationRef.push({sentBy:localMsisdn, text:'<widget name="git" filename="text.html" parameters="text=WOW&height=h1"/>'});
+}
