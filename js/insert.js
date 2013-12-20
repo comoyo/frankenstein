c8dd501bc1321f372664975f9d3433aee0f83801
sergey <sergey@comoyo.com>
2013-12-20 11:33:29 +0100

    wip

--- js/insert.js
+++ js/insert.js
@@ -1,6 +1,9 @@
 function ins_wow(dataRef, localMsisdn, partnerMsisdn) {
      conversationRef.push({sentBy:localMsisdn, text:'<widget name="git" filename="text.html" parameters="text=WOW&height=h1"/>'});
 }
+function ins_text(text, dataRef, localMsisdn, partnerMsisdn) {
+     conversationRef.push({sentBy:localMsisdn, text:'<widget name="git" filename="text.html" parameters="text=' + text + '&height=h1"/>'});
+}
 function ins_todo(dataRef, localMsisdn, partnerMsisdn) {
     var params = "partnerMsisdn=" + parnterMsisdn + "&localMsisdn=" + localMsisdn + 
         "&uniqueId=" + new Date().getTime();
