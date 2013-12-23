fb1b3c07dd0e50de179196e231fb440fb8c59f5d
sergey <sergey@comoyo.com>
2013-12-23 10:21:45 +0100

    added menu part

--- js/insert.js
+++ js/insert.js
@@ -2,11 +2,10 @@ function ins_wow(dataRef, localMsisdn, partnerMsisdn) {
      conversationRef.push({sentBy:localMsisdn, text:'<widget name="git" filename="text.html" parameters="text=WOW&height=h1"/>'});
 }
 function ins_text(text, dataRef, localMsisdn, partnerMsisdn) {
-     conversationRef.push({sentBy:localMsisdn, text:'<widget name="git" filename="text.html" parameters="text=' + text + '&height=h1"/>'});
+     conversationRef.push({sentBy:localMsisdn, text:'<widget name="git" filename="text.html" parameters="text=' + escape(text) + '&height=h1"/>'});
 }
 function ins_todo(dataRef, localMsisdn, partnerMsisdn) {
     var params = "partnerMsisdn=+" + partnerMsisdn.trim() + "&localMsisdn=+" + localMsisdn.trim() + 
         "&uniqueId=" + new Date().getTime();
-    console.log('params:' + params);
      conversationRef.push({sentBy:localMsisdn, text:'<widget name="git" filename="todo.html" parameters="' + params + '"/>'});
 }
